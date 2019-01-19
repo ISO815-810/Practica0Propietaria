c85165dc53dab60b636154a8f8b53c39e3adf8b5
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2019-01-19 01:06:45 -0800

    Creacion del proyecto Pull para leer archivo generado por el programa Push

---
+++ Practica0Pull/Practica0Pull/Application/LeerLayout.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica0Pull.Application
+{
+    class LeerLayout
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
