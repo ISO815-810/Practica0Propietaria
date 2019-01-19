56dc208b405b615a0bfc63934d7bfef20e3cdf61
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2019-01-19 01:54:22 -0800

    creacion de paquetes para estructura del proyecto. Creacion de la Clase ConnectionDB donde estara la conexion y las consultas. Creacion de la clase LeerLayout, en la que estara el metodo main

---
+++ Practica0Pull/Practica0Pull/Conexion/ConnectionDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica0Pull.Conexion
+{
+    class ConnectionDB
+    {
+
+        static string connectionString = @"Data Source=DELL-PC\MSSQLSERVER2016;Initial Catalog=TSS;Integrated Security=True";
+
+        static string SELECT_NOMINA = @"SELECT * FROM Nomina;";
+        static string SELECT_DETALLE_NOMINA = @"SELECT * FROM Detalle_Nomina;";
+
+        static string INSERT_NOMINA = @"INSERT INTO Nomina VALUES(@TipoRegistro,@Rnc,@PeriodoNomina);";
+        static string INSERT_DETALLE_NOMINA = @"INSERT INTO Detalle_Nomina VALUES(@TipoRegistro,@Cedula,@Sueldo,@Numero_TSS);";
+
+
+
+    }
+}
